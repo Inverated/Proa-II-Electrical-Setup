f16d916f4813c56a341d6560d585afb450275516
Inverated <120025059+Inverated@users.noreply.github.com>
2026-01-25 23:48:23 +0800

    Untested receiver module

--- Microcontroller Codes/ESP8266/digital_twin_ver_1/receiver_module/receiver_module.ino
+++ Microcontroller Codes/ESP8266/digital_twin_ver_1/receiver_module/receiver_module.ino
@@ -1,14 +1,39 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
+#include <map>
+
+// Buffer
+int FREQ = 1000; // ms
+
+// Battery array details
+int SERIES = 2;
+int PARALLEL = 1;
+float BATTERY_VOLTAGE = 25700 * SERIES;     // mV
+float BATTERY_CAPACITY = 50000 * PARALLEL;  // mAh
+float current_level = 50000 * PARALLEL;     // mAh (Ideally, use voltage - capacity to map initially)
 
 typedef struct {
-  int time_passed;
-  float mppt_current;
-  float voltage;
-  float HESensorOutput;
+  int time_passed;      // ms
+  float mppt_current;   // mA
+  float voltage;        // mV
+  float HESensorOutput; // A
 } MPPT_Sensor_Data;
 
 MPPT_Sensor_Data data;
+
+// Input Count
+int NUMBER_OF_ESP = 2;
+
+// Device Info
+typedef struct {
+  MPPT_Sensor_Data latest_data;
+  int time_passed; 
+  int last_recorded_time;
+} ESP_Device_Info;
+
+std::map<String, ESP_Device_Info> registeredDevices;
+
+unsigned long last_recorded_time = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -26,8 +51,24 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  float total_mppt_out = 0;
+  float total_load_in = 0;
+  if (registeredDevices.size() >= NUMBER_OF_ESP) {
+    for (const auto& device : registeredDevices) {
+      ESP_Device_Info device_info = device.second;
+      MPPT_Sensor_Data data = device_info.latest_data;
+      if ((device_info.time_passed - device_info.last_recorded_time) > FREQ) {
+        float time_passed_since_rec = device_info.time_passed - device_info.last_recorded_time;
+        total_mppt_out += data.mppt_current * time_passed_since_rec;
+        total_load_in += data.HESensorOutput * time_passed_since_rec;
+        device_info.last_recorded_time = device_info.time_passed;
+      }
+    }
+  }
+  float calculated = total_mppt_out - total_load_in;
+  current_level += min(calculated, BATTERY_CAPACITY);
 
+  Serial.print(current_level);  
 }
 
 void onDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
@@ -38,9 +79,24 @@ void onDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
               mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
   
   String macAddr = String(macChars);
+  ESP_Device_Info dev;
+  dev.latest_data = data;
+  dev.time_passed = millis();
+  dev.last_recorded_time = millis();
+
+  if (registeredDevices.find(macAddr) != registeredDevices.end()) {
+    // Do not reset the last recorded time
+    dev.last_recorded_time = registeredDevices[macAddr].last_recorded_time;
+  }
+
+  Serial.println(dev.time_passed);  
+  registeredDevices[macAddr] = dev;
+
   Serial.print("\nReceived from: ");
   Serial.println(macAddr);
-  
+}
+
+void printData(MPPT_Sensor_Data data) {  
   Serial.print("Time Passed: ");
   Serial.println(data.time_passed);
   
