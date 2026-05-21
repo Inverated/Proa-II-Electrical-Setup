d0afa034fe1a58275bf18fb5c4f298d0c551c8fd
Inverated <120025059+Inverated@users.noreply.github.com>
2026-05-22 02:09:39 +0800

    Test

--- Microcontroller Codes/ESP32/serial_reader_ads8688/receiver.py
+++ Microcontroller Codes/ESP32/serial_reader_ads8688/receiver.py
@@ -14,7 +14,7 @@ exists = csv_file_path.exists()
 csv_file = open(csv_file_path, mode='a', newline='')
 writer = csv.writer(csv_file)
 if not exists:
-    writer.writerow(['counter', 'time_ms', 'ch0', 'ch1', 'ch2',
+    writer.writerow(['counter', 'timestamp', 'timediff_us', 'ch0', 'ch1', 'ch2',
             'ch3', 'ch4', 'ch5', 'ch6', 'ch7', 'crc'])
     csv_file.flush()
 
@@ -59,20 +59,21 @@ while True:
                     del serial_device
                     time.sleep(1)
                 serial_device = serial.Serial(port.device, 2000000, timeout=1)
-                  
+                
             except serial.SerialException as e:
                 print(f"Failed to open {port.device}: {e}")
                 continue
-
             serial_device.reset_input_buffer()
             serial_device.reset_output_buffer()
             found = False
 
             for _ in range(50):
                 serial_device.write(b"START\n")
+                serial_device.flush()
                 try:
                     line = serial_device.readline().decode('utf-8').strip()
                 except:
+                    print("Ready flag not found. Checking for valid packet...")
                     if len(serial_device.readline()) == PACKET_SIZE:
                         found = True
                         break
@@ -88,25 +89,36 @@ while True:
 
             print(f"Connected to {port.device}")
             reading_chunk = 1
+            has_broken_packet = False
+            last_valid_counter = -1
+            buffered_alert_counter = 0
             
             while True:
                 packet = read_packet(serial_device)
+                
+                # Stagger alert notification                
                 waiting = serial_device.in_waiting
-                if waiting // PACKET_SIZE > 5000:
-                    print(f"Bytes waiting in buffer: {waiting}  ({waiting // PACKET_SIZE} packets behind)")
+                if waiting // PACKET_SIZE > 500:
+                    buffered_alert_counter += 1
+                    if buffered_alert_counter % 10 == 0:
+                        print(f"Bytes waiting in buffer: {waiting}  ({waiting // PACKET_SIZE} packets behind)")
+                else:
+                    buffered_alert_counter = 0
+                    
                 if packet is None:
                     print("No packet received. Retrying...")
                     continue
                 if len(packet) != PACKET_SIZE:
                     print(f"Expected {PACKET_SIZE} bytes, but got {len(packet)}. Skipping packet.")
                     print("Incomplete packet received. Skipping.")
+                    has_broken_packet = True
                     continue
 
                 """
-                    struct Packet {
-                        uint8_t header;
+                    struct __attribute__((packed)) Packet {
+                        uint16_t header;
                         uint32_t counter;
-                        uint32_t timeSinceStart;
+                        uint32_t timediff_us;		// Use micros. Sampling 1 / ms, use us for precision; 32bit -> ~70 min
                         uint16_t readings[8];
                         uint16_t crc;
                     };
@@ -116,24 +128,44 @@ while True:
                     # Returns a tuple with the data split into the specified size
                 except struct.error as e:
                     print(f"Error unpacking packet: {e}")
+                    has_broken_packet = True
                     continue
                 
                 header = data[0]
                 if (header != HEADER):
                     print(f"Invalid header: {header}. Expected {HEADER}. Skipping packet.")
+                    has_broken_packet = True
                     continue
                 
                 counter = data[1]
-                time_ms = data[2]
+                timediff_us = data[2]
                 readings = data[3:11]
                 crc = data[11]
-                                
-                calculated_crc = crc16(struct.pack('<HII8H', header, counter, time_ms, *readings))
+                now = time.time()
+                
+                raw_bytes = struct.pack('<HII8H', header, counter, timediff_us, *readings)
+                calculated_crc = crc16(raw_bytes)
                 if calculated_crc != crc:
-                    print(f"CRC mismatch at counter {counter}.")
+                    print(f"CRC mismatch at counter {counter}. Calculated: {calculated_crc}, Received: {crc}. Skipping packet.")
+                    has_broken_packet = True
                     continue
                 
-                writer.writerow([counter, time_ms, *readings, crc])
+                COUNTER_DIFF_THRESHOLD = 1000
+                if has_broken_packet and last_valid_counter != -1:
+                    if counter > last_valid_counter:
+                        timediff_us *= (counter - last_valid_counter)
+                    elif counter < last_valid_counter: # Counter reset and wraps around to 0
+                        wrap_around_diff = (0xFFFFFFFF - last_valid_counter) + counter + 1
+                        if wrap_around_diff < COUNTER_DIFF_THRESHOLD:
+                            timediff_us *= (counter + (0xFFFFFFFF - last_valid_counter) + 1)    
+                    else:
+                        print(f"Counter jump from {last_valid_counter} to {counter} is too large. Can't recover.")
+
+                    has_broken_packet = False
+                    
+                last_valid_counter = counter
+                writer.writerow([counter, timediff_us, *readings, crc])
+                
                 if reading_chunk >= COUNT_BEFORE_FLUSH:
                     csv_file.flush()
                     print(f"Saved {reading_chunk} packets.")
--- Microcontroller Codes/ESP32/serial_reader_ads8688/serial_reader_ads8688.ino
+++ Microcontroller Codes/ESP32/serial_reader_ads8688/serial_reader_ads8688.ino
@@ -1,4 +1,5 @@
 #include <ADS8688.h>
+#include "esp_rom_crc.h"
 
 #define PIN_CS 7
 #define PIN_SCK 6
@@ -11,39 +12,18 @@
 
 ADS8688 adc(PIN_CS, PIN_SCK, PIN_MOSI, PIN_MISO);
 
+// Speed Test
+// Max rate on board = 160000SPS
+// Max rate transfering via serial = 80000SPS
+
+// _attribute_((packed)) forces packet size to be whatever I set. Used prev when 8bit was used but not sure if required now
 struct __attribute__((packed)) Packet {
-    uint16_t header;
-    uint32_t counter;
-    uint32_t timeSinceStart;
-    uint16_t readings[8];
-    uint16_t crc;
+	uint16_t header;
+	uint32_t counter;
+	uint32_t timediff_us;		// Use micros. Sampling 1 / ms, use us for precision; 32bit -> ~70 min
+	uint16_t readings[8];
+	uint16_t crc;
 };
-
-const uint16_t PACKET_SIZE = 1000;
-Packet bulkPackets[PACKET_SIZE];
-
-// Default Vref = 4.096V
-// R1: Min = -(1.25 * vref)     = -5.120 V,     Max = +(1.25 * vref)    = +5.120 V
-// R5: Min = 0.0 V,                             Max = +(2.5 * vref)     = +10.240 V
-
-uint8_t r1_pins[6] = { 0, 1, 2, 3, 4, 5 };  // Direct HE sensor reading
-uint8_t r5_pins[2] = { 6, 7 };              // Battery voltage stepped down
-
-uint32_t prevTime;
-uint32_t counter = 1;
-uint16_t packet_position = 0;  // Up to 65536 bulk packet size
-bool streamEnabled = false;
-
-void checkForStart() {
-    if (Serial.available()) {
-        String cmd = Serial.readStringUntil('\n');
-        if (cmd == "START") {
-        Serial.println("ADC Ready");
-        Serial.flush();
-        streamEnabled = true;
-        }
-    }
-}
 
 uint16_t crc16(const uint8_t* data, uint16_t len) {
     uint16_t crc = 0xFFFF;
@@ -62,76 +42,104 @@ uint16_t crc16(const uint8_t* data, uint16_t len) {
     return crc;
 }
 
+const uint16_t PACKET_SIZE = 1000;
+Packet bulkPackets[PACKET_SIZE];
+
+// Default Vref = 4.096V
+// R1: Min = -(1.25 * vref)     = -5.120 V,     Max = +(1.25 * vref)    = +5.120 V
+// R5: Min = 0.0 V,                             Max = +(2.5 * vref)     = +10.240 V
+
+const uint8_t R1_SIZE = 6;
+const uint8_t R5_SIZE = 2;
+const uint8_t r1_pins[R1_SIZE] = { 0, 1, 2, 3, 4, 5 };  // Direct HE sensor reading
+const uint8_t r5_pins[R5_SIZE] = { 6, 7 };              // Battery voltage stepped down
+
+uint32_t prevTime_us;
+uint32_t counter = 1;
+uint16_t packet_position = 0;  // Up to 65536 bulk packet size (Max SRAM only allow ~8k size packet size only)
+bool streamEnabled = false;
+
+void checkForStart() {
+	if (Serial.available()) {
+		String cmd = Serial.readStringUntil('\n');
+		if (cmd == "START") {
+			Serial.println("ADC Ready");
+			Serial.flush();
+			streamEnabled = true;
+		}
+	}
+}
+
 void setup() {
-    //Serial.begin(921600);
-    Serial.begin(2000000);  // Jtag should ignore the set rate and just do maximum
+	//Serial.begin(921600);
+	Serial.begin(2000000);  // Jtag should ignore the set rate and just do maximum
 
-    for (int i = 0; i < sizeof(r1_pins) / sizeof(r1_pins[0]); i++) {
-        adc.setChannelRange(r1_pins[i], R1);
-    }
+	for (int i = 0; i < R1_SIZE; i++) {
+		adc.setChannelRange(r1_pins[i], R1);
+	}
 
-    for (int i = 0; i < sizeof(r5_pins) / sizeof(r5_pins[0]); i++) {
-        adc.setChannelRange(r5_pins[i], R5);
-    }
+	for (int i = 0; i < R5_SIZE; i++) {
+		adc.setChannelRange(r5_pins[i], R5);
+	}
 
-    // Enable all 8 channels in auto scan
-    adc.setChannelSequence(0xFF);  // or 0b11111111 (not x but b)
-    adc.setSampleRate(1000);
+	// Enable all 8 channels in auto scan
+	adc.setChannelSequence(0xFF);  // or 0b11111111 (not x but b)
+	adc.setSampleRate(0);
 
-    // Start auto scan mode
-    adc.autoRst();
-    adc.autoRst();
-    Serial.flush();
+	// Start auto scan mode
+	adc.autoRst();
+	adc.autoRst();
 
-    prevTime = millis();
+	prevTime_us = micros();
 }
 
 void loop() {
-    if (!streamEnabled) {
-        checkForStart();
-        return;
-    }
+	if (!streamEnabled) {
+		checkForStart();
+		return;
+	}
 
-    adc.waitForSample();
+	adc.waitForSample();
 
-    SPI.beginTransaction(SPISettings(ADS8688_SPI_CLOCK, MSBFIRST, SPI_MODE1));
+	SPI.beginTransaction(SPISettings(ADS8688_SPI_CLOCK, MSBFIRST, SPI_MODE1));
 
-    for (int i = 0; i < sizeof(r1_pins) / sizeof(r1_pins[0]); i++) {
-        uint8_t idx = r1_pins[i];
-        uint16_t raw = adc.noOpRaw() - DEFAULT_OFFSET;
-        //Serial.printf("CH%d: %5u \n", idx, raw);
-        bulkPackets[packet_position].readings[idx] = raw;
-    }
+	for (int i = 0; i < R1_SIZE; i++) {
+		uint8_t idx = r1_pins[i];
+		uint16_t raw = adc.noOpRaw() - DEFAULT_OFFSET;
+		bulkPackets[packet_position].readings[idx] = raw;
+	}
 
-    for (int i = 0; i < sizeof(r5_pins) / sizeof(r5_pins[0]); i++) {
-        uint8_t idx = r5_pins[i];
-        uint16_t raw = adc.noOpRaw();
-        if (raw < DIVIDER_OFFSET) {
-        raw = 0;
-        } else {
-        raw -= DIVIDER_OFFSET;
-        }
-        //Serial.printf("CH%d: %5u \n", idx, raw);
-        bulkPackets[packet_position].readings[idx] = raw;
-    }
-    SPI.endTransaction();
+	for (int i = 0; i < R5_SIZE; i++) {
+		uint8_t idx = r5_pins[i];
+		uint16_t raw = adc.noOpRaw();
+		if (raw < DIVIDER_OFFSET) {
+			raw = 0;
+		} else {
+			raw -= DIVIDER_OFFSET;
+		}
+		bulkPackets[packet_position].readings[idx] = raw;
+	}
+	SPI.endTransaction();
 
-    Packet& currPkt = bulkPackets[packet_position];
-    currPkt.header = HEADER;
-    currPkt.counter = counter;
-    currPkt.timeSinceStart = millis() - prevTime;
-    currPkt.crc = crc16((uint8_t*)&currPkt, sizeof(Packet) - sizeof(uint16_t));
+	uint32_t now_us = micros();
 
-    counter += 1;
-    packet_position += 1;
+	Packet& currPkt = bulkPackets[packet_position];
+	currPkt.header = HEADER;
+	currPkt.counter = counter;
+	currPkt.timediff_us = now_us - prevTime_us;
+	currPkt.crc = crc16((uint8_t*)&currPkt, sizeof(Packet) - sizeof(uint16_t));
 
-    if ((counter - 1) % PACKET_SIZE == 0) {
-        if (!Serial) {
+	prevTime_us = now_us;
+	counter += 1;
+	packet_position += 1;
+
+	if ((counter - 1) % PACKET_SIZE == 0) {
+		if (!Serial) {
 			streamEnabled = false;
 			return;
-        }
+		}
 
-        Serial.write((uint8_t*)&bulkPackets, sizeof(bulkPackets));
-        packet_position = 0;
-    }
+		Serial.write((uint8_t*)&bulkPackets, sizeof(bulkPackets));
+		packet_position = 0;
+	}
 }
