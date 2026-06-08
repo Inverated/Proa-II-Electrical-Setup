5d05ae85dc1b278ebe472d57483f77ddcaae8d7e
KQF <120025059+Inverated@users.noreply.github.com>
2026-06-08 17:45:18 +0800

    broken discharge data

--- Microcontroller Codes/ESP32/serial_reader_ads8688/receiver.py
+++ Microcontroller Codes/ESP32/serial_reader_ads8688/receiver.py
@@ -39,7 +39,7 @@ current_data = {ch: deque(maxlen=MAX_POINTS) for ch in CURRENT_CHANNELS}
 fig_voltage, ax_voltage = plt.subplots()
 #ax_voltage.set_ylim(0, 60)
 #ax_voltage.set_ylim(45, 60)
-ax_voltage.set_ylim(20, 30)
+ax_voltage.set_ylim(15, 30)
 voltage_lines = []
 for ch in VOLTAGE_CHANNELS:
     line, = ax_voltage.plot([], [], lw=0.5, label=f"ch{ch}")
@@ -50,7 +50,7 @@ last_time = 0
 # Current plotter (channels 0-5)
 fig_current, ax_current = plt.subplots()
 #ax_current.set_ylim(-100, 100)
-ax_current.set_ylim(-15, 15)
+ax_current.set_ylim(-10, 40)
 current_lines = []
 for ch in CURRENT_CHANNELS:
     line, = ax_current.plot([], [], lw=0.5, label=f"ch{ch}")
